1808c2a67ef2ce4c755582a319cf0f6110e073a2
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2018-10-18 17:00:10 +0300

    feat(docs): update docs according to ng add feature

--- demo/src/app/components/+datepicker/datepicker-section.list.ts
+++ demo/src/app/components/+datepicker/datepicker-section.list.ts
@@ -50,6 +50,7 @@ export const demoComponentContent: ContentSection[] = [
         component: require('!!raw-loader?lang=typescript!./demos/basic/basic.ts'),
         html: require('!!raw-loader?lang=markup!./demos/basic/basic.html'),
         description: `
+          <p>Note: If you installed ngx-bootstrap not through ng add command, you need to produce a few actions.</p>
           <p>Notable change is additional css for it <code> "/datepicker/bs-datepicker.css"</code> <br></p>
           <p>There are two ways of adding css:</p>
           <ul>
